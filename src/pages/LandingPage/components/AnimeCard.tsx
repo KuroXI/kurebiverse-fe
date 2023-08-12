c34b21c6b08162d0ab15d5eb19aa60356c3a9e78
KuroXI <vgonzales5157@gmail.com>
2023-08-12 12:56:19 +0800

    [landing-page] Fix carousel and hero component

--- src/pages/LandingPage/components/AnimeCard.tsx
+++ src/pages/LandingPage/components/AnimeCard.tsx
@@ -1,3 +1,5 @@
+import {Box} from "@mui/material";
+
 type AnimeCardType = {
   id: string;
   title: {
@@ -11,13 +13,15 @@ type AnimeCardType = {
   releaseDate: string;
 }
 
-const AnimeCard = ({title, image}: AnimeCardType) => {
+export default function AnimeCard({title, image}: AnimeCardType) {
   return (
-    <img
-      src={image}
-      alt={title.english || title.romaji || title.native}
-    />
+      <Box className={"relative max-w-[200px] max-h-[300px] min-w-[150px] min-h-[200px] w-[20vw] h-[26vw] inline-block mr-2"}>
+        <img
+          src={image}
+          alt={title.english || title.romaji || title.native}
+          sizes={"(min-width: 1080px) 200px, (min-width: 800px) calc(15.38vw + 37px), 146px"}
+          className={"h-full w-full cursor-pointer rounded-lg"}
+        />
+      </Box>
   );
-};
-
-export default AnimeCard;
+}
--- src/pages/LandingPage/components/Hero.tsx
+++ src/pages/LandingPage/components/Hero.tsx
@@ -1,78 +1,53 @@
-import { Info, PlayArrow } from "@mui/icons-material";
-import { Box, CircularProgress } from "@mui/material";
-import { useGetRandomAnimeQuery } from "../../../redux/services/animeapi";
+import {Box, CircularProgress} from "@mui/material";
+import {useGetRandomAnimeQuery} from "../../../redux/services/animeapi";
+import {Info, PlayArrow} from "@mui/icons-material";
 
-const Hero = () => {
-  const { data, isLoading } = useGetRandomAnimeQuery(null);
+export default function Hero() {
+  const {data, isLoading} = useGetRandomAnimeQuery(null);
 
-  if (data === null) {
-    window.location.reload();
-  }
+  if (data === null) window.location.reload();
 
-  return (
-    <Box className="bg-[#141414] xl:h-[90vh] lg:h-[80vh] md:h-[70vh] sm:h-[60vh] h-[50vh] flex justify-start items-center text-white relative">
-      {isLoading ? (
-        <Box className="flex justify-center items-center w-full">
-          <CircularProgress color="success" />
+  return isLoading ? (
+    <header className={"relative xl:h-[80vh] lg:h-[70vh] md:h-[60vh] sm:h-[50vh] h-[40vh]"}>
+      <Box className="flex justify-center items-center w-full h-full">
+        <CircularProgress color="success"/>
+      </Box>
+    </header>
+  ) : (
+    <header
+      className={"relative xl:h-[80vh] lg:h-[70vh] md:h-[60vh] sm:h-[50vh] h-[40vh] text-white object-contain"}
+      style={{
+        backgroundImage: `url(${data?.cover || data?.image})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center top"
+      }}
+    >
+      <Box className="absolute xl:pl-16 md:pl-10 sm:pl-7 pl-4 text-xl md:pt-[170px] pt-[100px]">
+        <h2 className="font-bold xl:text-5xl lg:text-4xl md:text-3xl sm:text-2xl">{data?.title}</h2>
+        <Box className="xl:text-xl lg:text-lg md:text-md sm:text-sm text-xs">
+          <Box className="flex gap-5 my-3">
+            <p className="text-[#6adf34] font-bold">{data?.rating} Rating</p>
+            <p>{data?.releaseDate}</p>
+            <p>{data?.totalEpisodes || 1} episode(s)</p>
+          </Box>
+          <p className="xl:w-1/2 md:w-2/4 w-3/4 line-clamp-3">{data?.description}</p>
         </Box>
-      ) : (
-        <>
-          <Box
-            className="absolute w-full xl:h-[90vh] lg:h-[80vh] md:h-[70vh] sm:h-[60vh] h-[50vh] flex items-center justify-center"
-            style={{
-              boxShadow: "0 0 5px 20px #141414",
-              backgroundColor: "#141414",
-              backgroundPosition: "center center",
-            }}
-          >
-            <div className="absolute top-0 left-0 h-full w-full pointer-events-none bg-gradient-to-r from-[#141414] to-transparent"></div>
-            <div className="absolute top-0 left-0 h-full w-full pointer-events-none bg-gradient-to-t from-[#141414] to-transparent"></div>
-            <img
-              src={data?.cover || data?.image}
-              alt="video-demo"
-              className="object-cover xl:h-[90vh] lg:h-[80vh] md:h-[70vh] sm:h-[60vh] h-[50vh] w-full"
-            />
-          </Box>
-          <Box className="xl:pl-16 md:pl-10 sm:pl-7 pl-4 text-xl z-20">
-            <h2 className="font-bold xl:text-5xl lg:text-4xl md:text-3xl sm:text-2xl">
-              {data?.title}
-            </h2>
-            <Box className="xl:text-xl lg:text-lg md:text-md sm:text-sm text-xs">
-              <Box className="flex gap-5 my-3">
-                <p className="text-[#6adf34] font-bold">
-                  {data?.rating} Rating
-                </p>
-                <p>{data?.releaseDate.split("-")[0]}</p>
-                <p>{data?.totalEpisodes || 1} episode(s)</p>
-              </Box>
-
-              <p className="xl:w-1/8 md:w-2/4 w-3/4 line-clamp-3">
-                {data?.description}
-              </p>
-            </Box>
-            <Box className="flex justify-start items-center text-center gap-5 mt-10">
-              <button className="xl:px-10 xl:py-5 lg:px-7 lg:py-4 px-4 py-2 bg-white rounded-sm font-bold ">
-                <p className="text-black xl:text-xl lg:text-lg md:text-md sm:text-sm text-xs">
-                  <span className="mr-2">
-                    <PlayArrow className="text-xl" />
-                  </span>
-                  Watch Episode 1
-                </p>
-              </button>
-              <button className="xl:px-10 xl:py-5 lg:px-7 lg:py-4 px-4 py-2 bg-[#424242] rounded-sm font-bold ">
-                <p className="xl:text-xl lg:text-lg md:text-md sm:text-sm text-xs">
-                  <span className="mr-2">
-                    <Info />
-                  </span>
-                  More Information
-                </p>
-              </button>
-            </Box>
-          </Box>
-        </>
-      )}
-    </Box>
+        <Box className="flex justify-start items-center text-center gap-5 md:mt-10 mt-5">
+          <button className="xl:px-10 xl:py-5 lg:px-7 lg:py-4 px-4 py-2 bg-white rounded-sm font-bold">
+            <p className="text-black xl:text-xl lg:text-lg md:text-md sm:text-sm text-xs">
+              <span className="mr-2"><PlayArrow className="text-xl"/></span>
+              Watch Episode 1
+            </p>
+          </button>
+          <button className="xl:px-10 xl:py-5 lg:px-7 lg:py-4 px-4 py-2 bg-[#424242] rounded-sm font-bold">
+            <p className="xl:text-xl lg:text-lg md:text-md sm:text-sm text-xs">
+              <span className="mr-2"><Info/></span>
+              More Information
+            </p>
+          </button>
+        </Box>
+      </Box>
+      <Box className="h-full w-full pointer-events-none bg-gradient-to-b from-transparent to-[#141414]"/>
+    </header>
   );
-};
-
-export default Hero;
+}
