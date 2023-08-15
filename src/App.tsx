1290dce5e81a9ca4bef451279ed18ab9572afacd
jgandrade <johnglenn.andrade@gmail.com>
2023-08-15 16:02:04 +0800

    [latest-page] Finish Latest Episodes

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Box } from "@mui/material";
 import { Route, Routes } from "react-router-dom";
 import { LandingPage, PopularPage, TrendingPage } from "./pages";
 import { Navbar } from "./components/Navbar";
+import { LatestEpisodesPage } from "./pages/LatestEpisodesPage";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/trending" element={<TrendingPage />} />
         <Route path="/popular" element={<PopularPage />} />
+        <Route path="/latest" element={<LatestEpisodesPage />} />
       </Routes>
     </Box>
   );
