98ea26fc018e58e6ee33dc9640df7741da8af185
KuroXI <vgonzales5157@gmail.com>
2023-08-26 19:29:59 +0800

    [api-setup] Modifying the route endpoints

--- src/redux/services/animeapi.ts
+++ src/redux/services/animeapi.ts
@@ -1,45 +1,49 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import {AnimeInfo} from "../../type/AnimeInfo.ts";
-import {AnimeRandom} from "../../type/AnimeRandom.ts";
-import {AnimeSchedule} from "../../type/AnimeSchedule.ts";
-import {AnimeSearch} from "../../type/AnimeSearch.ts";
-import {AnimeSort} from "../../type/AnimeSort.ts";
-import {AnimeStream} from "../../type/AnimeStream.ts";
+import { IResult, IAnime, IEpisode, ISource, IAiringSchedule } from "@kuroxi/kurebiverse-types";
 
 export type QueryPagePerPage = {
   page: number;
   perPage: number;
 };
 
+export interface AiringScheduleQuery extends QueryPagePerPage {
+  airingAtLesser: number
+  airingAtGreater: number
+}
+
 export const animeApi = createApi({
   reducerPath: "animeApi",
   baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_BASE_URL }),
   endpoints: (builder) => ({
-    getSearchAnime: builder.query<AnimeSearch, string>({
+    getSearchAnime: builder.query<IResult<IAnime>, string>({
       query: (searchString) => `/search/${searchString}`,
     }),
-    getAnimeInfo: builder.query<AnimeInfo, string>({
+    getAnimeInfo: builder.query<IAnime, string>({
       query: (animeId) => `/info/${animeId}`,
     }),
-    getAnimeStreamingLinks: builder.query<AnimeStream, string>({
+    getAnimeEpisodes: builder.query<IEpisode, string>({
+      query: (animeId) => `/info/${animeId}/episodes`,
+    }),
+    getAnimeStreamingLinks: builder.query<ISource, string>({
       query: (episodeId) => `/episode/${episodeId}`,
     }),
-    getAnimeRecentEpisodes: builder.query<AnimeSort, QueryPagePerPage>({
+    getAnimeUpcomingEpisodes: builder.query<IResult<IAnime>, QueryPagePerPage>({
       query: ({ page, perPage }: QueryPagePerPage) =>
-        `/airing?page=${page}&perPage=${perPage}`,
+        `/upcoming?page=${page}&perPage=${perPage}`,
     }),
-    getTrendingAnime: builder.query<AnimeSort, QueryPagePerPage>({
+    getTrendingAnime: builder.query<IResult<IAnime>, QueryPagePerPage>({
       query: ({ page, perPage }: QueryPagePerPage) =>
         `/trending?page=${page}&perPage=${perPage}`,
     }),
-    getPopularAnime: builder.query<AnimeSort, QueryPagePerPage>({
+    getPopularAnime: builder.query<IResult<IAnime>, QueryPagePerPage>({
       query: ({ page, perPage }: QueryPagePerPage) =>
         `/popular?page=${page}&perPage=${perPage}`,
     }),
-    getAiringSchedule: builder.query<AnimeSchedule, unknown>({
-      query: () => `/schedule`,
+    getAiringSchedule: builder.query<IAiringSchedule, AiringScheduleQuery>({
+      query: ({ page, perPage, airingAtGreater, airingAtLesser}) =>
+        `/schedule?page=${page}&perPage=${perPage}&airingAtGreater=${airingAtGreater}&airingAtLesser=${airingAtLesser}`,
     }),
-    getRandomAnime: builder.query<AnimeRandom, unknown>({
+    getRandomAnime: builder.query<IAnime, unknown>({
       query: () => `/random`,
     }),
   }),
@@ -49,7 +53,7 @@ export const {
   useGetSearchAnimeQuery,
   useGetAiringScheduleQuery,
   useGetAnimeInfoQuery,
-  useGetAnimeRecentEpisodesQuery,
+  useGetAnimeUpcomingEpisodesQuery,
   useGetAnimeStreamingLinksQuery,
   useGetPopularAnimeQuery,
   useGetRandomAnimeQuery,
