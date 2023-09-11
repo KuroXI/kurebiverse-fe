10c91d8dd19b1b0511b6ee4795e9b805162f6576
KuroXI <vgonzales5157@gmail.com>
2023-09-11 21:11:50 +0800

    [branch-db-structure] Update the History Row from a new Database Structure

--- src/pages/LandingPage/components/HistoryCard.tsx
+++ src/pages/LandingPage/components/HistoryCard.tsx
@@ -7,10 +7,10 @@ type HistoryCardProps = {
 }
 
 export default function HistoryCard({ anime } : HistoryCardProps) {
-  const { data } = useGetAnimeInfoQuery(anime.animeId);
+  const { data } = useGetAnimeInfoQuery(anime.anime_id);
 
   return data && (
-    <a href={`/watch/${anime.animeId}?episodeId=${anime.episodeId}`}>
+    <a href={`/watch/${anime.anime_id}?episodeId=${anime.episode_id}`}>
       <div
         className={"relative w-[28vw] h-[15vw] max-w-[450px] max-h-[250px] min-w-[220px] min-h-[150px] rounded-md"}
         style={{
@@ -26,23 +26,23 @@ export default function HistoryCard({ anime } : HistoryCardProps) {
               {data.title.english || data.title.userPreferred || data.title.romaji || data.title.native}
             </h1>
             <h1 className={"text-secondary-foreground text-sm"}>
-              Episode {anime.current_episode}
+              Episode {anime.episode_number}
             </h1>
           </div>
           <div className={"flex flex-col"}>
             <div className={"flex justify-between items-center"}>
               <h1 className={"text-secondary-foreground text-sm"}>
-                {parseTime(anime.time.progress * 1000)}
+                {parseTime(anime.current_duration * 1000)}
               </h1>
               <h1 className={"text-secondary-foreground text-sm"}>
-                {parseTime(anime.time.totalDuration * 1000)}
+                {parseTime(anime.total_duration * 1000)}
               </h1>
             </div>
             <div className={"relative"}>
               <div className={"absolute bg-primary-foreground rounded-full w-full h-2"}/>
               <div
                 className={`absolute bg-primary rounded-full h-2`}
-                style={{ width: `${progressBar(anime.time.progress, anime.time.totalDuration)}%` }}
+                style={{ width: `${progressBar(anime.current_duration, anime.total_duration)}%` }}
               />
             </div>
           </div>
