ba543bb6235caacc409ee6f42563569b485c84fc
jgandrade <johnglenn.andrade@gmail.com>
2023-10-07 09:39:19 +0800

    [pwa] fix pwa

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,16 @@ import { VitePWA } from "vite-plugin-pwa";
 import path from "path";
 
 export default defineConfig({
-  plugins: [react(), VitePWA()],
+  plugins: [react(), VitePWA({ registerType: "autoUpdate", manifest: {
+    icons: [
+      {
+        src: "/applogo.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "any maskable"
+      }
+    ]
+  } })],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
@@ -13,7 +22,7 @@ export default defineConfig({
   build: {
     rollupOptions: {
       output: {
-        format: 'esm',
+        format: "esm",
       },
     },
   },
